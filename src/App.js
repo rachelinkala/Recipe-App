56fb026843855fc21321e4813758825995641b0f
Rachel Inkala <rachelinkala@gmail.com>
2019-01-18 16:33:50 -0600

    display recipe names

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import Form from './components/Form';
 
 class App extends Component {
+  state = {
+    recipes: []
+  }
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
     const API_KEY = process.env.REACT_APP_RECIPE_API_KEY;
     const api_call = await fetch
-    (`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken`);
+    (`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=10`);
     
     const data = await api_call.json();
-    console.log(data);
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
   }
   render() {
     return (
@@ -20,6 +24,9 @@ class App extends Component {
           <h1 className="App-title"> Recipe Search </h1>
         </header>
         <Form getRecipe={this.getRecipe} />
+        { this.state.recipes.map((recipe) => {
+          return <p>{ recipe.title }</p>
+        }) }
       </div>
     );
   }
