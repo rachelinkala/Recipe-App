761ca69c57c9b32596c6711a7dcc3d00f28e97ba
Rachel Inkala <rachelinkala@gmail.com>
2019-01-18 16:39:16 -0600

    search bar returning matching titles

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     e.preventDefault();
     const API_KEY = process.env.REACT_APP_RECIPE_API_KEY;
     const api_call = await fetch
-    (`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=10`);
+    (`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
     
     const data = await api_call.json();
     this.setState({ recipes: data.recipes });
@@ -25,7 +25,7 @@ class App extends Component {
         </header>
         <Form getRecipe={this.getRecipe} />
         { this.state.recipes.map((recipe) => {
-          return <p>{ recipe.title }</p>
+          return <p key={recipe.recipe_id}>{ recipe.title }</p>
         }) }
       </div>
     );
