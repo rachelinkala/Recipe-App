4c007995caf9d8a792a6c453562be323d7291f01
Rachel Inkala <rachelinkala@gmail.com>
2019-01-22 15:56:49 -0600

    fix format error

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ class App extends Component {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
     const API_KEY = process.env.REACT_APP_RECIPE_API_KEY;
-    const api_call = await fetch
-    (`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
-    
+    const api_call = await fetch (`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
     const data = await api_call.json();
     this.setState({ recipes: data.recipes });
     console.log(this.state.recipes);
