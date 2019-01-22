b2d8fea7762a4a8079e70c4cc3f1e0056ffd3c49
Rachel Inkala <rachelinkala@gmail.com>
2019-01-22 15:55:13 -0600

    style tags

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -6,7 +6,7 @@ const Recipes = props => (
       { props.recipes.map((recipe) => {
         return (
           <div key={recipe.title} className="col-md-4" style={{ marginBottom:"2rem" }} >
-            <div className="recipe_box">
+            <div className="recipes__box">
               <img 
                 className="recipe__box-img" 
                 src={recipe.image_url} 
@@ -16,7 +16,7 @@ const Recipes = props => (
                 <h5 className="recipes__title">{ recipe.title }</h5>
                 <p className="recipes__subtitle">Publisher: <span> { recipe.publisher} </span></p>
               </div>
-              <button>View Recipe</button>
+              <button className="recipe_buttons">View Recipe</button>
             </div>
           </div>
         );
