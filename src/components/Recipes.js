afd375b7cf4fb1083da60a0b7791396227fe53e8
Rachel Inkala <rachelinkala@gmail.com>
2019-01-22 15:50:06 -0600

    recipes component styling

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
 const Recipes = props => (
-  <div>
-    { props.recipes.map((recipe) => {
-      return (
-        <div key={recipe.recipe_id}>
-          <img src={recipe.image_url} alt={recipe.title} />
-          <p>{ recipe.title }</p>
-        </div>
-      );
-    }) }
+  <div className="container">
+    <div className="row">
+      { props.recipes.map((recipe) => {
+        return (
+          <div key={recipe.title} className="col-md-4" style={{ marginBottom:"2rem" }} >
+            <div className="recipe_box">
+              <img 
+                className="recipe__box-img" 
+                src={recipe.image_url} 
+                alt={recipe.title} 
+              />
+              <div className="recipe__text">
+                <h5 className="recipes__title">{ recipe.title }</h5>
+                <p className="recipes__subtitle">Publisher: <span> { recipe.publisher} </span></p>
+              </div>
+              <button>View Recipe</button>
+            </div>
+          </div>
+        );
+      }) }
+    </div>
   </div>
-)
+);
 
 export default Recipes;
